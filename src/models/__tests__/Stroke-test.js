3c2440be9d39704ce5989286cf3e8d8e30ad26ad
David Chanin <dchanin@quizlet.com>
2015-10-07 18:25:33 -0600

    updating tests

---
+++ src/models/__tests__/Stroke-test.js
@@ -0,0 +1,21 @@
+const Stroke = require('../Stroke');
+
+import Point from '../Point';
+import StrokePart from '../StrokePart';
+
+describe('Stroke', () => {
+  it('should use the minimum distance to a stroke part when calculating distance', () => {
+    const points = [
+      new Point(1, 1),
+      new Point(1, 4),
+      new Point(5, 4),
+      new Point(5, 1),
+    ];
+    const strokeParts = [
+      new StrokePart(StrokePart.BACK_SLASH_STROKE, points),
+      new StrokePart(StrokePart.HORIZONTAL_STROKE, points),
+    ];
+
+    expect(new Stroke(strokeParts, 0).getDistance(new Point(6,6))).toBe(1);
+  });
+});
--- src/models/__tests__/StrokePart-test.js
+++ src/models/__tests__/StrokePart-test.js
@@ -3,7 +3,7 @@ const StrokePart = require('../StrokePart');
 import Point from '../Point';
 
 describe('StrokePart', () => {
-  it('should properly get starting points for each stroke type', () => {
+  it('should get starting points based on the stroke type', () => {
     const points = [
       new Point(1, 1),
       new Point(1, 5),
@@ -16,10 +16,11 @@ describe('StrokePart', () => {
     expect(new StrokePart(StrokePart.FORWARD_SLASH_STROKE, points).getStartingPoint()).toEqual(new Point(5,1));
     expect(new StrokePart(StrokePart.REVERSE_HORIZONTAL_STROKE, points).getStartingPoint()).toEqual(new Point(5,3));
     expect(new StrokePart(StrokePart.REVERSE_BACK_SLASH_STROKE, points).getStartingPoint()).toEqual(new Point(5,5));
-    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getStartingPoint()).toEqual(new Point(3, 5));
+    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getStartingPoint()).toEqual(new Point(3,5));
     expect(new StrokePart(StrokePart.REVERSE_FORWARD_SLASH_STROKE, points).getStartingPoint()).toEqual(new Point(1,5));
   });
-  it('should properly get ending points for each stroke type', () => {
+
+  it('should get ending points based on the stroke type', () => {
     const points = [
       new Point(1, 1),
       new Point(1, 5),
@@ -32,7 +33,42 @@ describe('StrokePart', () => {
     expect(new StrokePart(StrokePart.FORWARD_SLASH_STROKE, points).getEndingPoint()).toEqual(new Point(1,5));
     expect(new StrokePart(StrokePart.REVERSE_HORIZONTAL_STROKE, points).getEndingPoint()).toEqual(new Point(1,3));
     expect(new StrokePart(StrokePart.REVERSE_BACK_SLASH_STROKE, points).getEndingPoint()).toEqual(new Point(1,1));
-    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getEndingPoint()).toEqual(new Point(3, 1));
+    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getEndingPoint()).toEqual(new Point(3,1));
     expect(new StrokePart(StrokePart.REVERSE_FORWARD_SLASH_STROKE, points).getEndingPoint()).toEqual(new Point(5,1));
+  });
+
+  it('should calculate length of the stroke based on start and end points', () => {
+    const points = [
+      new Point(1, 1),
+      new Point(1, 4),
+      new Point(5, 4),
+      new Point(5, 1),
+    ];
+    expect(new StrokePart(StrokePart.HORIZONTAL_STROKE, points).getLength()).toBe(4);
+    expect(new StrokePart(StrokePart.BACK_SLASH_STROKE, points).getLength()).toBe(5);
+    expect(new StrokePart(StrokePart.VERTICAL_STROKE, points).getLength()).toBe(3);
+    expect(new StrokePart(StrokePart.FORWARD_SLASH_STROKE, points).getLength()).toBe(5);
+    expect(new StrokePart(StrokePart.REVERSE_HORIZONTAL_STROKE, points).getLength()).toBe(4);
+    expect(new StrokePart(StrokePart.REVERSE_BACK_SLASH_STROKE, points).getLength()).toBe(5);
+    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getLength()).toBe(3);
+    expect(new StrokePart(StrokePart.REVERSE_FORWARD_SLASH_STROKE, points).getLength()).toBe(5);
+  });
+
+  it('should calculate distance based on a line between start and end points', () => {
+    const points = [
+      new Point(1, 1),
+      new Point(1, 4),
+      new Point(5, 4),
+      new Point(5, 1),
+    ];
+    const refPoint = new Point(6,6);
+    expect(new StrokePart(StrokePart.HORIZONTAL_STROKE, points).getDistance(refPoint)).toBe(4);
+    expect(new StrokePart(StrokePart.BACK_SLASH_STROKE, points).getDistance(refPoint)).toBe(1);
+    expect(new StrokePart(StrokePart.VERTICAL_STROKE, points).getDistance(refPoint)).toBe(3);
+    expect(new StrokePart(StrokePart.FORWARD_SLASH_STROKE, points).getDistance(refPoint)).toBe(4.6);
+    expect(new StrokePart(StrokePart.REVERSE_HORIZONTAL_STROKE, points).getDistance(refPoint)).toBe(4);
+    expect(new StrokePart(StrokePart.REVERSE_BACK_SLASH_STROKE, points).getDistance(refPoint)).toBe(1);
+    expect(new StrokePart(StrokePart.REVERSE_VERTICAL_STROKE, points).getDistance(refPoint)).toBe(3);
+    expect(new StrokePart(StrokePart.REVERSE_FORWARD_SLASH_STROKE, points).getDistance(refPoint)).toBe(4.6);
   });
 });

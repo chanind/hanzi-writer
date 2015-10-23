e0a7310be031dd014fff0b101fe33929e633bc4b
David Chanin <dchanin@quizlet.com>
2015-10-22 22:28:00 -0500

    WIP

--- test.js
+++ test.js
@@ -1,45 +1,20 @@
-var writer;
-var isCharVisible;
-var isHintVisible;
-
-function updateCharacter() {
-	document.querySelector('#target').innerHTML = '';
-
-	var character = document.querySelector('.js-char').value
-	writer = new HanziWriter('target', character, {
-		width: 400,
-		height: 400
-	});
-	writer.showHint();
-	writer.showCharacter();
-	isCharVisible = true;
-	isHintVisible = true;
-	window.writer = writer;
-}
-
 window.onload = function() {
-	updateCharacter();
+	window.han = new HanziWriter('han', '汉', {
+		width: 200,
+		height: 200,
+		padding: 0,
+		strokeColor: '#fff',
+		hintColor: '#000',
+	});
+	han.animateCharacter({onComplete: han.animateCharacter.bind(han) });
 
-	document.querySelector('.js-char-form').addEventListener('submit', function(evt) {
-		evt.preventDefault();
-		updateCharacter();
+	window.zi = new HanziWriter('zi', '子', {
+		width: 200,
+		height: 200,
+		padding: 0,
+		strokeColor: '#fff',
+		hintColor: '#000',
 	});
+	zi.animateCharacter({onComplete: zi.animateCharacter.bind(zi) });
 
-	document.querySelector('.js-toggle').addEventListener('click', function() {
-		isCharVisible ? writer.hideCharacter() : writer.showCharacter();
-		isCharVisible = !isCharVisible;
-	});
-	document.querySelector('.js-toggle-hint').addEventListener('click', function() {
-		isHintVisible ? writer.hideHint() : writer.showHint();
-		isHintVisible = !isHintVisible;
-	});
-	document.querySelector('.js-animate').addEventListener('click', function() {
-		writer.animateCharacter();
-	});
-	document.querySelector('.js-quiz').addEventListener('click', function() {
-		writer.quiz({
-			enforceStrokeOrder: true,
-			showHint: true
-		});
-	});
-}
+};

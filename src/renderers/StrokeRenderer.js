2ddb059e8a0dd76d8bc02863e40e688c40482499
David Chanin <chanindav@gmail.com>
2018-02-14 18:07:38 -0500

    deleting commented code

--- src/renderers/StrokeRenderer.js
+++ src/renderers/StrokeRenderer.js
@@ -41,37 +41,6 @@ function StrokeRenderer(stroke) {
   this.path2D = new global.Path2D(this._stroke.path);
 }
 
-// StrokeRenderer.prototype.mount = function(canvas, props) {
-//   const { usePolygonMasks } = props;
-//   this._path = svg.createElm('path');
-//   const maskType = usePolygonMasks ? 'clipPath' : 'mask';
-//   this._mask = svg.createElm(maskType);
-//   this._maskPath = svg.createElm('path');
-//   const maskId = `mask-${counter()}`;
-//   svg.attr(this._mask, 'id', maskId);
-
-//   svg.attr(this._path, 'd', this._stroke.path);
-//   this._path.style.opacity = 0;
-//   const maskAttr = usePolygonMasks ? 'clip-path' : 'mask';
-//   svg.attr(this._path, maskAttr, `url(#${maskId})`);
-
-//   this.extendedMaskPoints = extendStart(filterParallelPoints(this._stroke.points), STROKE_WIDTH / 2);
-//   svg.attr(this._maskPath, 'd', svg.getPathString(this.extendedMaskPoints));
-//   svg.attrs(this._maskPath, {
-//     stroke: '#FFFFFF',
-//     'stroke-width': STROKE_WIDTH,
-//     fill: 'none',
-//     'stroke-linecap': 'round',
-//     'stroke-linejoin': 'miter',
-//     'stroke-dasharray': `${this._maskPathLength},${this._maskPathLength}`,
-//   });
-
-//   this._mask.appendChild(this._maskPath);
-//   canvas.defs.appendChild(this._mask);
-//   canvas.svg.appendChild(this._path);
-//   return this;
-// };
-
 StrokeRenderer.prototype.render = function(ctx, props) {
   ctx.save();
 
@@ -90,26 +59,6 @@ StrokeRenderer.prototype.render = function(ctx, props) {
   ctx.stroke(this.path2D);
 
   ctx.restore();
-
-  // if (props.displayPortion !== this._oldProps.displayPortion) {
-  //   this._maskPath.style.strokeDashoffset = this._getStrokeDashoffset(props.displayPortion);
-  // }
-
-  // const color = this._getColor(props);
-  // if (color !== this._getColor(this._oldProps)) {
-  //   svg.attrs(this._path, {
-  //     fill: color,
-  //     stroke: color,
-  //   });
-  // }
-
-  // if (props.strokeWidth !== this._oldProps.strokeWidth) {
-  //   svg.attrs(this._path, { strokeWidth: props.strokeWidth });
-  // }
-
-  // if (props.opacity !== this._oldProps.opacity) {
-  //   this._path.style.opacity = props.opacity;
-  // }
 };
 
 StrokeRenderer.prototype._getStrokeDashoffset = function(displayPortion) {

38c8cfac08351687a1423f41bbc49394737d7f1c
will.zhang <will.zhang@cloudminds.com>
2018-08-09 08:19:44 +0800

    Fix _width issue

--- src/svg.js
+++ src/svg.js
@@ -42,7 +42,7 @@ Canvas.prototype.createSubCanvas = function() {
   return new Canvas(group, this.defs);
 };
 
-Canvas.init = (elmOrId, _width = '100%', _height = '100%') => {
+Canvas.init = (elmOrId, width = '100%', height = '100%') => {
   let svg;
   let elm = elmOrId;
   if (typeof elmOrId === 'string') {
@@ -55,7 +55,7 @@ Canvas.init = (elmOrId, _width = '100%', _height = '100%') => {
     svg = createElm('svg');
     elm.appendChild(svg);
   }
-  attrs(svg, {width: _width, height: _height});
+  attrs(svg, {width, height});
   const defs = createElm('defs');
   svg.appendChild(defs);
   return new Canvas(svg, defs);

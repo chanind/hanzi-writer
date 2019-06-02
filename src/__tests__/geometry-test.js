5e5438f2391a2ff9b9a5cc14826eb3371419b852
David Chanin <chanindav@gmail.com>
2019-06-02 14:32:16 +0100

    renaming private geometry funcs

--- src/__tests__/geometry-test.js
+++ src/__tests__/geometry-test.js
@@ -1,35 +1,35 @@
 const geometry = require('../geometry');
 
 describe('geometry', () => {
-  describe('extendPointOnLine', () => {
+  describe('_extendPointOnLine', () => {
     it('returns a point distance away from the end point', () => {
       const p1 = {x: 0, y: 0};
       const p2 = {x: 8, y: 6};
-      expect(geometry.extendPointOnLine(p1, p2, 5)).toEqual({x: 12, y: 9});
+      expect(geometry._extendPointOnLine(p1, p2, 5)).toEqual({x: 12, y: 9});
     });
 
     it('works with negative distances', () => {
       const p1 = {x: 0, y: 0};
       const p2 = {x: 8, y: 6};
-      expect(geometry.extendPointOnLine(p1, p2, -5)).toEqual({x: 4, y: 3});
+      expect(geometry._extendPointOnLine(p1, p2, -5)).toEqual({x: 4, y: 3});
     });
 
     it('works when p2 is before p1 in the line', () => {
       const p1 = {x: 12, y: 9};
       const p2 = {x: 8, y: 6};
-      expect(geometry.extendPointOnLine(p1, p2, 10)).toEqual({x: 0, y: 0});
+      expect(geometry._extendPointOnLine(p1, p2, 10)).toEqual({x: 0, y: 0});
     });
 
     it('works with vertical lines', () => {
       const p1 = {x: 2, y: 4};
       const p2 = {x: 2, y: 6};
-      expect(geometry.extendPointOnLine(p1, p2, 7)).toEqual({x: 2, y: 13});
+      expect(geometry._extendPointOnLine(p1, p2, 7)).toEqual({x: 2, y: 13});
     });
 
     it('works with vertical lines where p2 is above p1', () => {
       const p1 = {x: 2, y: 6};
       const p2 = {x: 2, y: 4};
-      expect(geometry.extendPointOnLine(p1, p2, 7)).toEqual({x: 2, y: -3});
+      expect(geometry._extendPointOnLine(p1, p2, 7)).toEqual({x: 2, y: -3});
     });
   });
 
@@ -98,7 +98,7 @@ describe('geometry', () => {
     });
   });
 
-  describe('filterParallelPoints', () => {
+  describe('_filterParallelPoints', () => {
     it('removes internal points that are on the line connecting the points on either side', () => {
       const points = [
         {x: 0, y: 0},
@@ -110,7 +110,7 @@ describe('geometry', () => {
         {x: 10, y: 3},
         {x: 11, y: 3},
       ];
-      expect(geometry.filterParallelPoints(points)).toEqual([
+      expect(geometry._filterParallelPoints(points)).toEqual([
         {x: 0, y: 0},
         {x: 6, y: 0},
         {x: 9, y: 3},
@@ -125,7 +125,7 @@ describe('geometry', () => {
         {x: 9, y: 3},
         {x: 11, y: 3},
       ];
-      expect(geometry.filterParallelPoints(points)).toEqual(points);
+      expect(geometry._filterParallelPoints(points)).toEqual(points);
     });
   });
 

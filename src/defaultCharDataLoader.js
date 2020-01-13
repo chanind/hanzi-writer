bc790f13dd8744bae6aba6aac56833d6bf1128a0
David Chanin <chanindav@gmail.com>
2020-01-13 22:22:36 +0000

    chore: fixing comment about cdn source

--- src/defaultCharDataLoader.js
+++ src/defaultCharDataLoader.js
@@ -2,7 +2,7 @@ const VERSION = '2.0';
 const getCharDataUrl = (char) => `https://cdn.jsdelivr.net/npm/hanzi-writer-data@${VERSION}/${char}.json`;
 
 module.exports = (char, onLoad, onError) => {
-  // load char data from hanziwriter.org cdn (currently hosted on github pages)
+  // load char data from hanziwriter cdn (currently hosted on jsdelivr)
   const xhr = new global.XMLHttpRequest();
   if (xhr.overrideMimeType) { // IE 9 and 10 don't seem to support this...
     xhr.overrideMimeType('application/json');

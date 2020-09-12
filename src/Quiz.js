685230c397b219d256e9c61c935fb148e7810ee2
jamsch <jamsch18@student.wintec.ac.nz>
2020-09-12 22:22:22 +1200

    update(Quiz) remove unused function param in "_handleSuccess"

--- src/Quiz.js
+++ src/Quiz.js
@@ -66,7 +66,7 @@ Quiz.prototype.endUserStroke = function() {
   });
 
   if (isMatch) {
-    this._handleSuccess(currentStroke);
+    this._handleSuccess();
   } else {
     this._handleFailure();
     if (this._numRecentMistakes >= this._options.showHintAfterMisses) {
@@ -85,7 +85,7 @@ Quiz.prototype.cancel = function() {
   }
 };
 
-Quiz.prototype._handleSuccess = function(stroke) {
+Quiz.prototype._handleSuccess = function() {
   callIfExists(this._options.onCorrectStroke, {
     character: this._character.symbol,
     strokeNum: this._currentStrokeIndex,

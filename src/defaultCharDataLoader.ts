ac60d7d7bf3f6351478df7825d3855c397bb9380
James <jamsch18@student.wintec.ac.nz>
2020-09-12 19:01:56 +1200

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

---
+++ src/defaultCharDataLoader.ts
@@ -0,0 +1,20 @@
+const VERSION = "2.0";
+const getCharDataUrl = (char) =>
+    `https://cdn.jsdelivr.net/npm/hanzi-writer-data@${VERSION}/${char}.json`;
+
+export default function defaultCharDataLoader(
+    char: string,
+    onLoad: (parsedJson: {}) => void,
+    onError: (error?: any) => void,
+) {
+    // load char data from hanziwriter cdn (currently hosted on jsdelivr)
+    fetch(getCharDataUrl(char))
+        .then((response) => {
+            if (!response.ok) {
+                Promise.reject(response);
+            }
+            return response.json();
+        })
+        .then(onLoad)
+        .catch(onError);
+}

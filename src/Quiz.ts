3de527fbfd047d0d3672d394c423622212f2593c
Valentin Herrmann <me@valentin-herrmann.de>
2024-05-21 14:01:57 +0200

    feat: add onCancel option to quizzes

--- src/Quiz.ts
+++ src/Quiz.ts
@@ -157,6 +157,16 @@ export default class Quiz {
         ),
       );
     }
+
+    if (!this._options) return;
+
+    const { symbol } = this._character;
+    const { onCancel } = this._options;
+
+    onCancel?.({
+      character: symbol,
+      totalMistakes: this._totalMistakes,
+    });
   }
 
   _getStrokeData({
